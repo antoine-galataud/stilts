e2b83c0c0eca8115d09a33c4d15fc985a4fd3250
Antoine Galataud <antoine@foobot.io>
2015-06-16 13:40:04 +0200

    Remove for client only

--- stomplet-server-core/src/test/java/org/projectodd/stilts/stomplet/BasicWebSocketStompletServerTest.java
+++
@@ -1,9 +0,0 @@
-package org.projectodd.stilts.stomplet;
-
-public class BasicWebSocketStompletServerTest extends BasicStompletServerTest {
-    
-    public String getConnectionUrl() {
-        return "stomp+ws://localhost/";
-    }
-
-}
