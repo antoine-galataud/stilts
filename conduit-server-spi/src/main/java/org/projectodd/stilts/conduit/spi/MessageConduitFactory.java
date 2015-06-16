bdcf3e897eda97b9d29d240f3cc50d1353c73ee2
Antoine Galataud <antoine@foobot.io>
2015-06-16 13:41:06 +0200

    Remove unnecessary project

--- conduit-server-spi/src/main/java/org/projectodd/stilts/conduit/spi/MessageConduitFactory.java
+++
@@ -1,9 +0,0 @@
-package org.projectodd.stilts.conduit.spi;
-
-import org.projectodd.stilts.stomp.Headers;
-import org.projectodd.stilts.stomp.spi.AcknowledgeableMessageSink;
-
-public interface MessageConduitFactory {
-
-    MessageConduit createMessageConduit(AcknowledgeableMessageSink messageSink, Headers headers) throws Exception;
-}
